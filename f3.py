80d2c961f84d371487dee69f50a77a1a7ec4ccb2
Sharan <sharan122040@gmail.com>
2026-07-03 10:40:48 +0530

    added code to the f3 & f4 py file to feature branch

---
+++ f3.py
@@ -0,0 +1,11 @@
+def sum_1(a,b):
+    return (a+b)
+sum_1(2,3)
+def sub_1(a,b):
+    return (a-b)
+
+def master(a1,b1):
+    ans_1=sum_1(a1,b1)
+    ans_2=sub_1(a1,b1)
+    print(ans_1,ans_2)
+master(4,8)    
---
+++ f4.py
@@ -0,0 +1,5 @@
+def sum_n(* param):
+    sum_1=0
+    for i in param:
+        sum_1+=i
+        return sum_1
